39f60b9d56d662afc9053378a55cc32fffc1e148
Juan Pablo Mata <134900297+juanpismata11@users.noreply.github.com>
2025-06-24 20:10:03 -0600

    Update ProveedoresView.xaml.cs

--- ProyectoMaui/Views/ProveedoresView.xaml.cs
+++ ProyectoMaui/Views/ProveedoresView.xaml.cs
@@ -1,4 +1,5 @@
 using ProyectoMaui.ViewModels;
+using ProyectoMaui.Models;
 using ProyectoMaui.Services;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 namespace ProyectoMaui.Views;
